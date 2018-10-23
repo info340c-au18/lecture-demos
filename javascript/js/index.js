2f2d5e1399d93907df2981b66163152501902e30
iGuest <timca@sports-it.com>
2018-10-23 13:12:10 -0700

    updates before class

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -27,9 +27,9 @@ function doTogether(firstCallback, secondCallback){
     console.log('at the same time!');
 }
 
-function patHead() {
-    console.log('pat your head');
-}
+// function patHead() {
+//     console.log('pat your head');
+// }
 
 function rubBelly() {
     console.log('rub your belly');
@@ -38,31 +38,66 @@ function rubBelly() {
 //pass in the callbacks to do them together
 doTogether(rubBelly, patHead);
 
+// let patHead = function() {
+//     console.log('pat your head');
+// }
 
+function sayHello(name) { 
+    console.log("Hello, "+name);
+ }
+ 
+ //takes ANOTHER FUNCTION as an arg
+ //will call the arg function, 
+ //passing it "world"
+ function doWorld(aFunction) {
+    aFunction("world");
+ }
+ 
+ 
+ 
+ //call function and pass value
+ doWorld(sayHello); //prints "Hello world"
+ 
+ 
+ //pass literal (anonymous value)
+ doWorld(function(msg) {
+   console.log("you said: "+msg);
+ }); //prints "you said: world"
+ 
 
-for(item of myArray){
+function patHead() {
+    console.log('pat your head');
+}
+
+let myArray1 = [1,2,3,4,5];
+
+for(item of myArray1){
     console.log(item);
 }
 
-let sum = 0
-myArray.forEach(function(n){
-    sum = sum + n
+let sum = 0;
+myArray1.forEach(function(n){
+    sum = sum + n;
 });
+console.log(sum);
 
 
-
-myArray.forEach(function(n, idx){
-    myArray[idx] = n*n
+myArray1.forEach(function(n, idx){
+    myArray1[idx] = n*n;
 });
+console.log(myArray1);
 
-myArray = myArray.map(function(n) {
-    return n*n
+myArray1 = myArray1.map(function(n) {
+    return n*n;
 })
+console.log(myArray1);
 
 let add = function(total, next){
     return total+next;
 }
 
-myArray.reduce(function(total, next){
+let myReduction = myArray1.reduce(function(total, next){
     return total+next;
 }, 5)
+console.log(myReduction);
+

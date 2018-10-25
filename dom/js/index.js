b5983403c827c62faf16c57299c075be0b05e3c6
Tim Carlson <timca@dashplatform.com>
2018-10-24 21:37:16 -0700

    updates for tomorrows class

--- dom/js/index.js
+++ dom/js/index.js
@@ -1,57 +1,67 @@
 'use strict';
 
-let theH1 = document.querySelector('h1');
-console.log(theH1)
+// let theH1 = document.querySelector('h1');
+// console.log(theH1)
 
-//change content
-let subtitle = document.querySelector('header p');
-subtitle.innerHTML = subtitle.textContent + " <em>Prof Ross</em>";
+// //change content
+// let subtitle = document.querySelector('header p');
+// subtitle.innerHTML = subtitle.textContent + " <em>Prof Carlson</em>";
 
-//change attributes
-let img = document.querySelector('img')
+ //change attributes
+ let img = document.querySelector('img');
+//  img.src = 'img/husky.jpg';
 
-//change style
-theH1.classList.add('text-success');
-img.classList.add('anim')
+// //change style
+// theH1.classList.add('text-success');
+// img.classList.add('anim')
 
-//create an element
-let newLi = document.createElement('li')
-newLi.innerHTML = '<a href="https://ischool.uw.edu/">Another iSchool Link</a>'
-let linkList = document.querySelector('ul')
-linkList.appendChild(newLi)
+// //create an element
+// let newLi = document.createElement('li')
+// newLi.innerHTML = '<a href="https://ischool.uw.edu/">Another iSchool Link</a>'
+// let linkList = document.querySelector('ul')
+// linkList.appendChild(newLi)
 
-function renderCookie() {
-    let jar = document.querySelector('#cookie-jar');
-    let cookie = document.createElement('img')
-    cookie.src = "img/cookie.png"
-    cookie.alt = "a cookie"
-    jar.appendChild(cookie);
-}
+// function renderCookie() {
+//     let jar = document.querySelector('#cookie-jar');
+//     let cookie = document.createElement('img')
+//     cookie.src = "img/cookie.png"
+//     cookie.alt = "a cookie"
+//     jar.appendChild(cookie);
+// }
 
-function renderCookieJar(number) {
-    let jar = document.querySelector('#cookie-jar')
-    jar.innerHTML = '';
-    for(let i=0; i<number; i++){
-        renderCookie()
-    }
-}
+// renderCookie();
 
-//actually do that
-renderCookieJar(10)
-renderCookieJar(5)
+// function renderCookieJar(number) {
+//     let jar = document.querySelector('#cookie-jar')
+//     jar.innerHTML = '';
+//     for(let i=0; i<number; i++){
+//         renderCookie()
+//     }
+// }
 
-let isPuppy = true
+// //actually do that
+// renderCookieJar(10);
 
-let button1 = document.querySelector('#button1')
-img.addEventListener('click', function() {
-    if(isPuppy){
-        img.src = 'img/husky.jpg';
-        isPuppy = false
-    } else {
-        img.src = 'img/puppy.jpg';
-        isPuppy = true
-    }
-})
+
+// // renderCookieJar(5);
+
+// let button1 = document.querySelector('#button1');
+// button1.addEventListener('click', function() {
+// console.log("button clicked")
+
+// });
+
+// let isPuppy = true;
+
+// img.addEventListener('click', function() {
+//     if(isPuppy){
+//         img.src = 'img/husky.jpg';
+//         isPuppy = false
+//     } else {
+//         img.src = 'img/puppy.jpg';
+//         isPuppy = true
+//     }
+// })
 
 
 
